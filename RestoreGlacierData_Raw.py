cb02c971b04e76806acb27180fe1a6ae7036cefe
rudrapbiswas <rudrapbiswas@gmail.com>
2022-10-30 20:57:31 +0530

    Add Python files
    Add Python files

---
+++ RestoreGlacierData_Raw.py
@@ -0,0 +1,93 @@
+#!/bin/python
+import sys
+import os
+import boto3
+
+# DEFINE CONSTANTS
+AWS_Access_Key = "AKIA3HIID3X5PCO5UP5S"
+AWS_Secret_Key = "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"
+REGION  = "us-east-1"
+S3_BUCKET  = "benbkt"
+FOLDER_PREFIX  = "be123/demo/2022-10-11T00-00Z"
+RESTORATION_LIFESPAN= 14
+
+file_list = []
+bucket = S3_BUCKET
+prefix = FOLDER_PREFIX
+
+#Create an S3 Client Session
+client = boto3.client(
+      's3',
+      aws_access_key_id=AWS_Access_Key,
+      aws_secret_access_key=AWS_Secret_Key,
+      region_name=REGION
+   )
+
+# Get List Of Glacier Objects
+def restoreGlacier(type):
+    EXIT_CODE = 0
+   # Try to get S3 list
+    try:
+      s3_result = client.list_objects_v2(
+         Bucket=bucket,
+         Prefix=prefix
+      )
+    except Exception as e:
+        print("Unable to list these files with error: ", e)
+        exit()
+    
+    if 'Contents' not in s3_result:
+      return []
+
+   # Get list of Contents
+    for file in s3_result['Contents']:
+      # Only Log Glacier Files
+        try:
+            print("Restoring file: ", file, "\n")
+            doRestoreContents(file, type, file_list)
+        except Exception as e:
+            EXIT_CODE = 1
+            print("Unable to list contents of file: ", file, " with error:\n", e)
+         
+   # Get list of Contents when More than 1000 Items
+    while s3_result['IsTruncated']:
+        continuation_key = s3_result['NextContinuationToken']
+        s3_result = s3_conn.list_objects_v2(Bucket=bucket, Prefix=prefix, Delimiter="/", ContinuationToken=continuation_key)
+        for file in s3_result['Contents']:
+            try:
+                print("Restoring file: ", file, "\n")
+                doRestoreContents(file, type, file_list)
+            except Exception as e:
+                EXIT_CODE = 1
+                print("Unable to list contents of file: ", file, " for more than 1000 items with error:\n", e)          
+
+    # Return File List
+    return EXIT_CODE
+
+def doRestoreContents(file, type, file_list):
+    resObject = client.restore_object(Bucket=bucket, Key=file['Key'],RestoreRequest={'Days': RESTORATION_LIFESPAN, 'GlacierJobParameters': {
+            'Tier': type}})
+    if file['StorageClass'] == 'GLACIER':
+        if type=="Bulk":
+            response = resObject
+        elif type=="Standard":
+            response = resObject
+            file_list.append(file['Key'])
+
+def glacierRestore(type):
+   if type=="Bulk":
+      op = restoreGlacier(type)
+      # If Bulk Success
+      if op == 0:
+        print("Bulk Request Success!", "Bulk Restore Request Successful! Your files will be restored in 5-12 hours.")
+   elif type=="Standard":
+      op = restoreGlacier(type)
+      # If Standard Success
+      if op == 0:
+          print("Standard Request Success!", "Standard Restore Request Successful! Your files will be restored in 3-5 hours.")
+   else:
+      # If Failure
+      print("Error", "There was an issue requesting restore of files, please contact your S3 administrator.")
+    
+if __name__ == '__main__':
+    glacierRestore("Bulk")
---
+++ RestoreGlacierData_Raw_UI.py
@@ -0,0 +1,163 @@
+#!/bin/python
+from tkinter import *
+from tkinter import messagebox
+import boto3
+import webbrowser
+import tkinter as tk
+
+# Initialize
+fields = ('AWS Access Key', 'AWS Secret Key','S3 Bucket (Format: bucketname)', 'Folder to Restore (Format: folder1/folder2/)', 'Bucket Region (Example: us-east-1)',)
+
+# Enable Hyperlinking
+def hyperlink(url):
+    webbrowser.open_new(url)
+
+# Create an S3 Client Session
+def create_client(entries):
+   client = boto3.client(
+      's3', 
+      aws_access_key_id=entries['AWS Access Key'].get(), 
+      aws_secret_access_key=entries['AWS Secret Key'].get(),
+      region_name=entries['Bucket Region (Example: us-east-1)'].get()
+   )
+   return client
+
+def restore_glacier_contents(entries, type):
+
+   client = create_client(entries)    
+
+   bucket = entries['S3 Bucket (Format: bucketname)'].get()
+   prefix = entries['Folder to Restore (Format: folder1/folder2/)'].get()
+
+   # Try to get S3 list
+   try:
+      s3_result = client.list_objects_v2(
+         Bucket=bucket,
+         Prefix=prefix
+      )
+   except:
+      messagebox.showerror("Error", "You are unable to list these files.")
+      exit()
+
+   if 'Contents' not in s3_result:
+      return []
+   file_list = []
+
+   # Get list of Contents
+   for file in s3_result['Contents']:
+      # Only Log Glacier Files
+      if file['StorageClass'] == 'GLACIER':
+         if type=="Bulk":
+            response = client.restore_object(Bucket=bucket, Key=file['Key'],RestoreRequest={'Days': 14, 'GlacierJobParameters': {
+            'Tier': 'Bulk'}})
+         elif type=="Standard":
+            response = client.restore_object(Bucket=bucket, Key=file['Key'],RestoreRequest={'Days': 14, 'GlacierJobParameters': {
+            'Tier': 'Standard'}})
+         file_list.append(file['Key'])
+
+   # Get list of Contents when More than 1000 Items
+   while s3_result['IsTruncated']:
+      continuation_key = s3_result['NextContinuationToken']
+      s3_result = s3_conn.list_objects_v2(Bucket=bucket, Prefix=prefix, Delimiter="/", ContinuationToken=continuation_key)
+      for file in s3_result['Contents']:
+            # Only Log Glacier Files
+         if file['StorageClass'] == 'GLACIER':
+            if type=="Bulk":
+                  response = client.restore_object(Bucket=bucket, Key=file['Key'],RestoreRequest={'Days': 14, 'GlacierJobParameters': {
+               'Tier': 'Bulk'}})
+            elif type=="Standard":
+               response = client.restore_object(Bucket=bucket, Key=file['Key'],RestoreRequest={'Days': 14, 'GlacierJobParameters': {
+               'Tier': 'Standard'}})
+            file_list.append(file['Key'])
+
+   # Return File List
+   return file_list
+
+def glacier_restore(entries, type):
+
+   if type=="Bulk":
+      files = restore_glacier_contents(entries, type)
+      print(files)
+      # If Bulk Success
+      messagebox.showinfo("Bulk Request Success!", "Bulk Restore Request Successful! Your files will be restored in 5-12 hours.")
+
+   elif type=="Standard":
+      files = restore_glacier_contents(entries, type)
+      #print(files)
+      # If Standard Success
+      messagebox.showinfo("Standard Request Success!", "Standard Restore Request Successful! Your files will be restored in 3-5 hours.")
+
+   else:
+      # If Failure
+      messagebox.showerror("Error", "There was an issue requesting restore of files, please contact your S3 administrator.")
+
+def makeform(root, fields):
+       
+   # Initialize Entries
+   entries = {}
+   
+   for field in fields:
+      row = Frame(root)
+
+      # Add Field Title
+      lab = Label(row, width=50, text=field+": ", anchor='w')
+
+      # Add Field Entry
+      if field=="AWS Secret Key":
+         # AWS Secret Key is sensitive data and should be obscured
+         ent = Entry(row, width=61, show="*")
+
+      else:  
+         ent = Entry(row, width=61)
+
+      # Add Styling
+      row.pack(side = TOP, fill = X, padx = 5 , pady = 5)
+      lab.pack(side = LEFT)
+      ent.pack(side = LEFT, expand = YES, fill = X)
+      
+      # Save to Entries
+      entries[field] = ent
+   return entries
+
+if __name__ == '__main__':
+       
+   # Init TK
+   root = Tk()
+
+   # Set Window Title
+   root.title("AWS Glacier Restore to S3")
+   lab1 = Label(root, text="AWS Glacier Restore to S3", font=('Helvetica', 14))
+   lab1.pack(side = TOP)
+
+   lab2 = Label(root, text="Restoration will take a few minutes as files restore 1 by 1, program is working even if it says it is Not Responding.", font=('Helvetica', 10))
+   lab2.pack(side = TOP)
+
+   # Add Hyperlink to AWS Region Documentation
+   link2 = Label(root, text="GitHub", fg="blue", cursor="hand2")
+   link2.pack(side = BOTTOM, fill = X, padx = 5 , pady = 5)
+   link2.bind("<Button-1>", lambda e: hyperlink("https://github.com/rudrapbiswas/DataRestoreFromAWSGlacier"))
+
+   # Add Hyperlink to AWS Region Documentation
+   link1 = Label(root, text="Documentation: AWS Region Codes", fg="blue", cursor="hand2")
+   link1.pack(side = TOP, fill = X, padx = 5 , pady = 5)
+   link1.bind("<Button-1>", lambda e: hyperlink("https://docs.aws.amazon.com/general/latest/gr/rande.html"))
+
+   # Init the Form
+   ents = makeform(root, fields)
+
+   # Add Quit Button
+   b3 = Button(root, text = 'Quit', command = root.quit)
+   b3.pack(side = RIGHT, padx = 5, pady = 5)
+
+   # Add Bulk Retrieval Button
+   b1 = Button(root, text = 'Bulk Retrieval (5-12 Hours)',
+   command=(lambda e = ents: glacier_restore(e, "Bulk")))
+   b1.pack(side = RIGHT, padx = 5, pady = 5)
+
+   # Add Standard Retrieval Button
+   b2 = Button(root, text='Standard Retrieval (3-5 Hours)',
+   command=(lambda e = ents: glacier_restore(e, "Standard")))
+   b2.pack(side = RIGHT, padx = 5, pady = 5)
+
+   # Run
+   root.mainloop()
